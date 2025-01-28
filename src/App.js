3d544f18cefc1393ad7e6f503f6219bf7f20f14a
Makan <contact@makantayebi.com>
2025-01-28 09:52:43 +0100

    syntactic correction

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Timeline from './Timeline';
 
 function App() {
   return (
--- src/Timeline.jsx
+++ src/Timeline.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './Timeline.css'
+
 // Sample data in (date, title) format
 const events = [
     { date: "2025-01-01", title: "New Year's Day" },
